a9bc5d02176aa5a375746fbd3a05ee53e8ab4882
JamilsonPereira <jamilson.nunesp@hotmail.com>
2021-10-11 11:02:12 -0300

    Implementado a classe de dominio Categoria

---
+++ src/main/java/br/com/nelioalves/domain/Categoria.java
@@ -0,0 +1,53 @@
+package br.com.nelioalves.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Categoria implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private Integer id;
+	private String nome;
+
+	public Categoria(Integer id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	// O hashcode busca o valor inteiro, então aqui ele irá gerar um numero inteiro
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	// Compara o objeto por valor e não pelo ponteiro em memoria. de forma booleana 
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
--- src/main/java/br/com/nelioalves/resources/CategoriaResource.java
+++ src/main/java/br/com/nelioalves/resources/CategoriaResource.java
@@ -1,18 +1,33 @@
 package br.com.nelioalves.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.nelioalves.domain.Categoria;
+
 //Anotação do Rest para controller 
 @RestController
-@RequestMapping("/categoria")
+@RequestMapping("/categorias")
 public class CategoriaResource {
 
-	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return  "Rest Funcionando";
+	@RequestMapping(method = RequestMethod.GET)
+	public List<Categoria> listar() {
+
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, "Escritorio");
+
+		// List é uma interface e não pode ser instanciada, então usamos o Arraylist
+		// para instancia-lo
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+
+		return lista;
 	}
-	
+
 }
